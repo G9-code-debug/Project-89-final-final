ff3e2e310ce9eb1dd274ac0e2f877688752026db
G9-code-debug <83263462+G9-code-debug@users.noreply.github.com>
2021-07-01 15:37:57 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,32 @@
+player1 = "";
+player2 = "";
+player1score = 0;
+player2score = 0;
+number1 = 0;
+number2 = 0;
+var xy = 0;
+player1 = localStorage.getItem("user_x");
+player2 = localStorage.getItem("user_y");
+document.getElementById("a").innerHTML = player1 + " = " + player1score;
+document.getElementById("c").innerHTML = player2 + " = " + player2score;
+document.getElementById("f").innerHTML = player1;
+document.getElementById("h").innerHTML = player2;
+abcd.style.display = "none";
+check.style.display = "none";
+function send() {
+    if(document.getElementById("x").value == "" && document.getElementById("y").value == "") {
+        alert("there wasn't any input");
+    }
+    else{
+    number1 = document.getElementById("x").value;
+    document.getElementById("x").value = "";
+    number2 = document.getElementById("y").value;
+    document.getElementById("y").value = "";
+    document.getElementById("l").innerHTML = number1 + " x " + number2;
+    document.getElementById("q").innerHTML = "Answer: "
+    abcd.style.display = "inline";
+    check.style.display = "inline";
+    xy = number1 * number2;
+    console.log(xy);
+    }
+} 
